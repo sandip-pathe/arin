6831b48f535a53f173affbed82784110748542ff
Sandip Pathe <sandippathe9689@gmail.com>
2025-07-03 09:32:23 +0000

    use Source Sans Pro for body use Playfair Display for heading

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Space Grotesk', 'sans-serif'],
+        body: ['Source Sans Pro', 'sans-serif'],
+        headline: ['Playfair Display', 'serif'],
         code: ['monospace'],
       },
       colors: {
